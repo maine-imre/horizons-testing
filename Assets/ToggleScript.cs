cd281a83545dc56a5287d3237f4232b40a0141c4
Rene Yost <rene.yost@maine.edu>
2019-05-20 10:17:21 -0400

    rene laptop init commit

--- Assets/ToggleScript.cs
+++ Assets/ToggleScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ToggleScript : MonoBehaviour
 {
+    //list of meshrenderers for shapes
+    //meshrenderer for active shape
     public List<MeshRenderer> shapes;
     public MeshRenderer cShape;
 
     public void Start()
     {
+        //loop through list and make each shape invisible
+        //initialize first shape to visible
         for (int i = 0; i < shapes.Count; i++)
             shapes[i].enabled = false;
         cShape = shapes[0];
@@ -17,6 +21,7 @@ public class ToggleScript : MonoBehaviour
 
     public void Update()
     {
+        //if right arrow pressed, go down list (wrap around at end)
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             cShape = shapes[(shapes.IndexOf(cShape) + 1) % shapes.Count];
@@ -24,6 +29,7 @@ public class ToggleScript : MonoBehaviour
             cShape.enabled = true;
         }
 
+        //if left arrow pressed, go back up list
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             cShape = shapes[(shapes.IndexOf(cShape) - 1) % shapes.Count];
